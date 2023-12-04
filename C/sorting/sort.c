2dc56145c881bc80f685c9e6a2d2cecb6be4d6c6
othsueh <ych930719@gmail.com>
2023-12-04 17:02:29 +0800

    finish quick sort

--- C/sorting/sort.c
+++ C/sorting/sort.c
@@ -1,9 +1,47 @@
 #include "../lib/base.h"
 #include "../lib/algobase.h"
-#define EelmenType i32
-void insertion_sort(EelmenType list[], i32 length)
+#define Elementype i32
+void printArray(Elementype list[], i32 length)
 {
-    EelmenType next;
+    for(i32 i = 0; i < length; i++)
+    {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+}
+void randomArray(Elementype *list, i32 length)
+{
+    srand((u32)time(NULL));
+    for(i32 i = 0; i < length; i++)
+    {
+        list[i] = rand() % 100;
+    }
+}
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(&arr[i], &arr[j]); // swap(arr[i], arr[j]
+            }
+            printArray(arr, 10);
+        }
+        swap(&arr[++i], &arr[high]); // The pivot is in the right place
+        int pi = i;
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+void insertion_sort(Elementype list[], i32 length)
+{
+    Elementype next;
     for(i32 i = 1; i < length; i++)
     {
         i32 j = i;
@@ -18,6 +56,10 @@ void insertion_sort(EelmenType list[], i32 length)
 }
 int main()
 {
-    EelmenType a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2 ,1};
-    insertion_sort(a, 10);
+    Elementype a[10] = {0};
+    randomArray(a, 10);
+    printf("Before sort: ");
+    printArray(a, 10); 
+    quickSort(a, 0, 9);
+    printArray(a, 10); 
 }
